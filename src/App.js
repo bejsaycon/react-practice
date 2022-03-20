403204ba1bd16724c1b7dabbf06207468e384c79
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-03-21 02:19:39 +0800

    changed css

--- src/App.js
+++ src/App.js
@@ -1,48 +1,44 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-//Custom hook
-// import { useForm } from "./useForm";
-// import { useFetch } from "./useFetch";
-
-
 function App() {
-  const [seed, setSeed] = useState('');
-  const [input, setInput] = useState('');
+  const [seed, setSeed] = useState(null);
+  const [input, setInput] = useState("");
   const [userData, setUserData] = useState([]);
-  // const [values, handleChange] = useForm({seed:""});
-  
-  const url = `https://randomuser.me/api/?seed=${seed}`
 
-  useEffect(()=>{
+  const url = `https://randomuser.me/api/?seed=${seed}`;
+
+  useEffect(() => {
     fetch(url)
-    .then(response => response.json())
-    .then(data=>{
+      .then((response) => response.json())
+      .then((data) => {
         setUserData(data);
-    })
-}, [seed])
+      });
+    return () => {
+      setInput("");
+    };
+  }, [url]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setSeed(input);
-  }
-
-
-  
+  };
 
   return (
     <div className="container">
       <form onSubmit={handleSubmit}>
-      <input  
-        className="input"
-        name="seed"
-        placeholder="Enter random string"
-        onChange={(e)=>setInput(e.target.value)}
-        value={input}
-      />
+        <input
+          className="input"
+          name="seed"
+          placeholder="Enter random string"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          required
+        />
+        <button type="submit"><i className="fas fa-search"></i></button>
       </form>
 
-      {typeof userData.results === "undefined" ? (
+      {seed === null ? (
         <div className="below-input-box">
           <p className="head-text" id="welcome-message">
             Welcome to Random user Generator, Enter any keyword to start
@@ -64,7 +60,7 @@ function App() {
               {userData.results[0].name.first} {userData.results[0].name.last}
             </p>
             <p id="address" className="user-details head-text">
-            <i className="fas fa-map-marker-alt"></i>{" "}
+              <i className="fas fa-map-marker-alt"></i>{" "}
               {userData.results[0].location.street.name}{" "}
               {userData.results[0].location.street.number}
               {", "}
@@ -76,13 +72,11 @@ function App() {
               {userData.results[0].location.country}
             </p>
             <p id="email" className="user-details head-text">
-            <i className="fas fa-envelope"></i>{" "}
-              {userData.results[0].email}
+              <i className="fas fa-envelope"></i> {userData.results[0].email}
             </p>
 
             <p id="phone" className="user-details head-text">
-            <i className="fas fa-phone-alt"></i>{" "}
-              {userData.results[0].phone}
+              <i className="fas fa-phone-alt"></i> {userData.results[0].phone}
             </p>
           </div>
         </div>
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 export const useFetch = (url) => {
+
+const [data, setData] = useState(null)
+
+
 useEffect(()=>{
     fetch(url)
     .then(response => response.json())
     .then(y=>{
-        console.log(y);
-    })
+        setData(y);
+    });
 }, [url])
+
+return data;
 }
